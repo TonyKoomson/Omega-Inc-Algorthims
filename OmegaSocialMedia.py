46bd03513c46d56ac5ea94ebc183a946fd387b39
Tony Koomson <tony.koomson0@gmail.com>
2024-12-12 18:41:49 +0000

    Revised Version Of Class Creation
    as title says this is a revised version of the creation of the class

--- OmegaSocialMedia.py
+++ OmegaSocialMedia.py
@@ -1,9 +1,36 @@
 from collections import defaultdict, deque
 import heapq
 
-Class OmegaSocialMedia;
-    def __init__(self);
-    self.users = {}
-    self.friendships = defaultdict(set)
-    self.feed =  []
-    self.max_feed_size = 1000
+class OmegaSocialMedia:
+    def __init__(self):
+        self.users = []:
+        self.frendships = {};
+        self.post = []:
+    
+    def register_user(self,username,email,phone_number):
+        # (not implemented yet)
+        pass
+    
+    def add_friendship(self, username, email, phone_number):
+        # (not implemented yet)
+        pass
+
+    def remove_friendship(self, user1, user2):
+        # (not implemented yet)
+        pass
+
+    def add_post(self,user,content):
+        # (not implemented yet)
+        pass
+
+    def get_user_posts(self, user);
+        # (not implemented yet)
+        pass
+
+    def recommend_friends(self, user):
+        # (not implemented yet)
+        pass
+    
+     def recommend_friends(self, user):
+        # (not implemented yet)
+        pass
