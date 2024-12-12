b07441a8cafa33f1251adec8a354336b7e746e21
Tony Koomson <tony.koomson0@gmail.com>
2024-12-12 19:22:56 +0000

    Added friendship functionality
    added the ability to remove and add friendships

--- OmegaSocialMedia.py
+++ OmegaSocialMedia.py
@@ -18,16 +18,37 @@ class OmegaSocialMedia:
                 'phone_number': phone_number
             }
             self.users.append(new_user)
+            self.frendships[username] = username
             print(f"User '{username}' registered succesfully.")
-        pass
+        
     
     def add_friendship(self, username, email, phone_number):
-        # (not implemented yet)
-        pass
+        if user1 not in self.friendships or user2 not in self.friendships:
+            print("Error: one or both users do not exist.")
+        return
+    
+        if user2 not in self.friendships or user2 not in self.friendships:
+            print("Error: One or both users do not exist.")
+            return
+        
+        if user2 not in self.frendships[user1]:
+            self.frendships[user1].append(user2)
+            self.add_friendships[user2].append(user1)
+            print(f"Users '{user1} and {user2} are now friends.")
+        else:
+            print(f"Error: Users '{user1}' and '{user2}are already friends.")
 
     def remove_friendship(self, user1, user2):
-        # (not implemented yet)
-        pass
+        if user1 not in self.friendship or user2 not in self.friendships:
+            print("Error: One or Both users does not exist")
+        return
+
+        if user2 in self.frendships[user1]:
+            self.frendships[user1].remove(user2)
+            self.frendships[user2].remove(user1)
+            print(f"Users '{user1}' and ' {user2}' are no longer friends")
+        else:
+            print(f"Error: Users '{user1}' and '{user2}' are not friends")
 
     def add_post(self,user,content):
         # (not implemented yet)
